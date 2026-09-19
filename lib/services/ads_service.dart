0e45c82c3a4f669c28d47a5f45c5efa28b9189fc
Sabroj Babu <sabrojdeewana@gmail.com>
2026-09-19 14:42:06 +0530

    Remove AdMob references

--- lib/services/ads_service.dart
+++ lib/services/ads_service.dart
@@ -1,54 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-
 class AdsService {
-  static const bannerId =
-      'ca-app-pub-3940256099942544/6300978111';
-
-  static const rewardedId =
-      'ca-app-pub-3940256099942544/5224354917';
-
-  BannerAd createBanner({required VoidCallback onLoaded}) {
-    final ad = BannerAd(
-      adUnitId: bannerId,
-      size: AdSize.banner,
-      request: const AdRequest(),
-      listener: BannerAdListener(
-        onAdLoaded: (_) => onLoaded(),
-        onAdFailedToLoad: (ad, _) => ad.dispose(),
-      ),
-    );
-
-    ad.load();
-    return ad;
-  }
-
-  void showRewarded({required VoidCallback onComplete}) {
-    RewardedAd.load(
-      adUnitId: rewardedId,
-      request: const AdRequest(),
-      rewardedAdLoadCallback: RewardedAdLoadCallback(
-        onAdLoaded: (ad) {
-          ad.fullScreenContentCallback =
-              FullScreenContentCallback(
-            onAdDismissedFullScreenContent: (ad) {
-              ad.dispose();
-              onComplete();
-            },
-            onAdFailedToShowFullScreenContent: (ad, _) {
-              ad.dispose();
-              onComplete();
-            },
-          );
-
-          ad.show(
-            onUserEarnedReward: (_, __) {},
-          );
-        },
-        onAdFailedToLoad: (_) {
-          onComplete();
-        },
-      ),
-    );
+  void showRewarded({required void Function() onComplete}) {
+    onComplete();
   }
 }
