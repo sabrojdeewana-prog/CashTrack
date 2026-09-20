0b8a1abedd598a3dcd4bd4646f770b5248b80e95
Sabroj Babu <sabrojdeewana@gmail.com>
2026-09-20 21:50:50 +0530

    Remove leftover Google Auth import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'services/google_auth_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
