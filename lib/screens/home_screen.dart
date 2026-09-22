05108fe350ae83003d8b59e87da17f810425d2f4
Sabroj Babu <sabrojdeewana@gmail.com>
2026-09-22 14:01:54 +0530

    Fix deprecated opacity warning

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -227,7 +227,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 borderRadius: BorderRadius.circular(16),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.04),
+                    color: Colors.black.withValues(alpha: 0.04),
                     blurRadius: 12,
                     offset: const Offset(0, 4),
                   ),
